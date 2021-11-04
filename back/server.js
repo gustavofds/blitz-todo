03a211ef164067f1e8cc45a195c7d8af8a8ae5db
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 20:08:18 -0300

    Instala mongoose e cria conexao com DB

--- back/server.js
+++ back/server.js
@@ -1,9 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
 
+const DATABASE_URL = process.env.DB_URL.replace(
+  '<password>',
+  process.env.DB_PASSWORD
+);
+
+mongoose.connect(DATABASE_URL).then(() => {
+  console.log('Connected to Database!');
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
