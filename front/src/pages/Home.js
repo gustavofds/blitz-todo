21c878abe9ac0f473de666433efd306c5ccac0d9
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 22:21:26 -0300

    Implementacao inicial da requisicao para /api/tasks

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import TaskList from '../components/TaskList';
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    axios.get('http://localhost:4000/api/tasks').then((data) => {
+      setTasks(data.data);
+      console.log(data.data);
+
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
       <h1>Ebytr - Lista de Tarefas</h1>
-      <TaskList />
+      {isLoading ? 'Carregando...' : <TaskList />}
     </>
   );
 }
