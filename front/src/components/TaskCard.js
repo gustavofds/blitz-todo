95d871be9cbe543c34829c02b5b8f9b59318ed8f
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 22:35:26 -0300

    Cria botao para deletar tarefa

--- front/src/components/TaskCard.js
+++ front/src/components/TaskCard.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
+
 function TaskCard({ task }) {
-  console.log(task);
+  const deleteTask = (task) => {
+    axios.delete(`http://localhost:4000/api/tasks/${task._id}`);
+  };
+
   return (
     <li>
-      {task.description} - {task.status}
+      {task.description} - {task.status}{' '}
+      <button onClick={() => deleteTask(task)}>Deletar</button>
     </li>
   );
 }
