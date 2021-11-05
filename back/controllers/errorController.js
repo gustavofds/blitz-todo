9dba61a47ce33d1428e32e01e69b5801109d93da
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 21:27:19 -0300

    Adiciona tratamento de ValidationError lancado no model

--- back/controllers/errorController.js
+++ back/controllers/errorController.js
@@ -5,5 +5,9 @@ module.exports = (err, req, res, _next) => {
     return res.status(err.code).json({ message: err.message });
   }
 
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ message: err.message });
+  }
+
   return res.status(500).json({ message: 'Something went wrong :(' });
 };
