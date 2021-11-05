73464fe8466b0859e3df987480d4db2d3b68fb3e
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 22:44:17 -0300

    Lista atualiza ao deletar tarefa

--- front/src/components/TaskCard.js
+++ front/src/components/TaskCard.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-function TaskCard({ task }) {
+function TaskCard({ task, setTasks }) {
   const deleteTask = (task) => {
-    axios.delete(`http://localhost:4000/api/tasks/${task._id}`);
+    axios
+      .delete(`http://localhost:4000/api/tasks/${task._id}`)
+      .then(() => axios.get('http://localhost:4000/api/tasks'))
+      .then((data) => setTasks(data.data));
   };
 
   return (
