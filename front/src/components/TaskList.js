1ac0b7f46efefccc5b2ddcccfeab20f753b965e1
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-04 22:14:00 -0300

    Implementacao inicial da TaskList

---
+++ front/src/components/TaskList.js
@@ -0,0 +1,5 @@
+function TaskList() {
+  return 'Isso é uma lista de tarefas';
+}
+
+export default TaskList;
---
+++ front/src/pages/Home.js
@@ -0,0 +1,12 @@
+import TaskList from '../components/TaskList';
+
+function Home() {
+  return (
+    <>
+      <h1>Ebytr - Lista de Tarefas</h1>
+      <TaskList />
+    </>
+  );
+}
+
+export default Home;
